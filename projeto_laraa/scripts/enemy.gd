c573278f6415fc94eac8f8309ef58f07ee30a31d
lara <130380646+LaraBC2007@users.noreply.github.com>
2024-07-01 17:37:39 -0300

    01/07 - 17:37

--- projeto_laraa/scripts/enemy.gd
+++ projeto_laraa/scripts/enemy.gd
@@ -4,8 +4,14 @@ extends RigidBody2D
 
 func _ready():
 	var enemy_types = anim.sprite_frames.get_animation_names()
+	anim.play(enemy_types[randi() % enemy_types.size()])
 	print(enemy_types)
 
 func _process(delta):
 	pass
+
+
+
+func _on_visible_screen_exited():
+	queue_free()
 
